015703d275519afa8443655b2f92022863fe335a
张浩鹏 <15275157828@163.com>
2017-12-07 17:45:46 +0800

    “t
    增加 status Bar 功能

--- src/main/java/com/mydemo/MainActivity.java
+++ src/main/java/com/mydemo/MainActivity.java
@@ -15,6 +15,7 @@ import com.mydemo.CircleImage.CircleImageActivity;
 import com.mydemo.DiyProgressBar.DiyProgressBarActivity;
 import com.mydemo.FlowViewActivity.FlowViewActivity;
 import com.mydemo.OkhttpUtils.TestOkhttp;
+import com.mydemo.StatusBarDemo.StatusBarTest;
 import com.mydemo.StickyHeadActivity.StickyHeadActivity;
 import com.mydemo.StudyRxJava.RxJavaActivity;
 import com.mydemo.SwipeMenuView.SwipeMenuViewActivity;
@@ -70,6 +71,7 @@ public class MainActivity extends BaseActivity {
                     case 5:// RxJava_RxAndroid学习
                         Intent goRxJavaIntent = new Intent(MainActivity.this, RxJavaActivity.class);
                         startActivity(goRxJavaIntent);
+                        break;
                     case 6:// okhttp封装使用
                         Intent goOkHttpIntent = new Intent(MainActivity.this, TestOkhttp.class);
                         startActivity(goOkHttpIntent);
@@ -81,6 +83,10 @@ public class MainActivity extends BaseActivity {
                     case 8:// 各种类型系统对话框学习
                         Intent goSystemDialogIntent = new Intent(MainActivity.this, SystemDialogActivity.class);
                         startActivity(goSystemDialogIntent);
+                        break;
+                    case 9:// status bar状态栏 学习; 5.0以下 &#45;&#45; 5.0以上区别及实现方式
+                        Intent goStatusBarTestIntent = new Intent(MainActivity.this, StatusBarTest.class);
+                        startActivity(goStatusBarTestIntent);
                         break;
                 }
             }
@@ -97,6 +103,7 @@ public class MainActivity extends BaseActivity {
         list.add(getResources().getString(R.string.okhttp));
         list.add(getResources().getString(R.string.carame_album));
         list.add(getResources().getString(R.string.system_dialog));
+        list.add(getResources().getString(R.string.statusbar));
 
     }
 
---
+++ src/main/java/com/mydemo/StatusBarDemo/StatusBarTest.java
@@ -0,0 +1,28 @@
+package com.mydemo.StatusBarDemo;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mydemo.Base.BaseActivity;
+import com.mydemo.R;
+
+/**
+ * Created by HaoPz on 2017/12/7.
+ */
+
+public class StatusBarTest extends BaseActivity {
+
+    private static final int COLOR_DEFAULT = Color.parseColor("#ff0000");
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_bar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(COLOR_DEFAULT);
+        }
+    }
+}
